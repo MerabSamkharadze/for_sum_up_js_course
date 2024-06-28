14df106866386b5c2ceb0f82dcb7d57a52a11fb7
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-28 13:16:15 +0400

    function

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Functions: Write functions to perform basic tasks such as calculating the area of a rectangle, converting temperatures, or reversing a string.
+
+function getRectArea(a, b, c) {
+  return `rectangle area is : ${a + b + c}`;
+}
